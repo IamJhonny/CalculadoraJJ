c1d2d24154be93e9101cf317aca0b526eb471c3e
JHONNY <jhonnymanuel2001@gmail.com>
2023-05-31 21:51:34 -0400

    first commit calculadora in VS

---
+++ CalculadoraJJ/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace CalculadoraJJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtN1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.JJ = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnPromedio = new System.Windows.Forms.Button();
+            this.btnEQL = new System.Windows.Forms.Button();
+            this.btnMenor = new System.Windows.Forms.Button();
+            this.btnMayor = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtN1
+            // 
+            this.txtN1.AutoSize = true;
+            this.txtN1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN1.Location = new System.Drawing.Point(6, 200);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(49, 16);
+            this.txtN1.TabIndex = 0;
+            this.txtN1.Text = "Num 1";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(73, 199);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(280, 20);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(73, 223);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(280, 20);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // txtN2
+            // 
+            this.txtN2.AutoSize = true;
+            this.txtN2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN2.Location = new System.Drawing.Point(6, 227);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(49, 16);
+            this.txtN2.TabIndex = 2;
+            this.txtN2.Text = "Num 2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CalculadoraJJ.Properties.Resources.Jhonny;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 157);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.textBox3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(179, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(320, 23);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "  JHONNY MANUEL DE LOS SANTOS 1-18-6915";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(179, 79);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(362, 23);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "Bienvenido a mi práctica favorita";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(179, 127);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(397, 23);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "Hola, soy jhonny te ayudaremos a resolver tus problemas";
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.BackColor = System.Drawing.Color.IndianRed;
+            this.btnSuma.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(9, 273);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(45, 45);
+            this.btnSuma.TabIndex = 8;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = false;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.BackColor = System.Drawing.Color.IndianRed;
+            this.btnResta.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResta.Location = new System.Drawing.Point(60, 273);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(45, 45);
+            this.btnResta.TabIndex = 9;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = false;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.IndianRed;
+            this.btnDividir.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(162, 273);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(45, 45);
+            this.btnDividir.TabIndex = 10;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.BackColor = System.Drawing.Color.IndianRed;
+            this.btnMultiplicar.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(111, 273);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(45, 45);
+            this.btnMultiplicar.TabIndex = 11;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = false;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // JJ
+            // 
+            this.JJ.AutoSize = true;
+            this.JJ.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.JJ.Location = new System.Drawing.Point(9, 377);
+            this.JJ.Name = "JJ";
+            this.JJ.Size = new System.Drawing.Size(72, 16);
+            this.JJ.TabIndex = 12;
+            this.JJ.Text = "Resultado";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(87, 377);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(266, 20);
+            this.txtResultado.TabIndex = 13;
+            // 
+            // btnPromedio
+            // 
+            this.btnPromedio.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPromedio.Location = new System.Drawing.Point(229, 273);
+            this.btnPromedio.Name = "btnPromedio";
+            this.btnPromedio.Size = new System.Drawing.Size(124, 45);
+            this.btnPromedio.TabIndex = 14;
+            this.btnPromedio.Text = "Promedio";
+            this.btnPromedio.UseVisualStyleBackColor = true;
+            this.btnPromedio.Click += new System.EventHandler(this.btnPromedio_Click);
+            // 
+            // btnEQL
+            // 
+            this.btnEQL.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEQL.Location = new System.Drawing.Point(209, 327);
+            this.btnEQL.Name = "btnEQL";
+            this.btnEQL.Size = new System.Drawing.Size(144, 35);
+            this.btnEQL.TabIndex = 15;
+            this.btnEQL.Text = "Eq literal del promedio";
+            this.btnEQL.UseVisualStyleBackColor = true;
+            this.btnEQL.Click += new System.EventHandler(this.btnEQL_Click);
+            // 
+            // btnMenor
+            // 
+            this.btnMenor.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenor.Location = new System.Drawing.Point(97, 327);
+            this.btnMenor.Name = "btnMenor";
+            this.btnMenor.Size = new System.Drawing.Size(92, 35);
+            this.btnMenor.TabIndex = 16;
+            this.btnMenor.Text = "Menor";
+            this.btnMenor.UseVisualStyleBackColor = true;
+            this.btnMenor.Click += new System.EventHandler(this.btnMenor_Click);
+            // 
+            // btnMayor
+            // 
+            this.btnMayor.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMayor.Location = new System.Drawing.Point(13, 327);
+            this.btnMayor.Name = "btnMayor";
+            this.btnMayor.Size = new System.Drawing.Size(78, 35);
+            this.btnMayor.TabIndex = 17;
+            this.btnMayor.Text = "Mayor";
+            this.btnMayor.UseVisualStyleBackColor = true;
+            this.btnMayor.Click += new System.EventHandler(this.btnMayor_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(577, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "---------------------------------------------------------------------------------" +
+    "--------------------------------------------------------------------------------" +
+    "-----------------------------";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 406);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnMayor);
+            this.Controls.Add(this.btnMenor);
+            this.Controls.Add(this.btnEQL);
+            this.Controls.Add(this.btnPromedio);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.JJ);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.txtN1);
+            this.Name = "Form1";
+            this.Text = "Calculadora JJ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtN1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label txtN2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Label JJ;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnPromedio;
+        private System.Windows.Forms.Button btnEQL;
+        private System.Windows.Forms.Button btnMenor;
+        private System.Windows.Forms.Button btnMayor;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CalculadoraJJ/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraJJ
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int.TryParse(txtNum1.Text, out num1);
+            int.TryParse(txtNum2.Text, out num2);
+
+            int res = num1 + num2;
+
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int.TryParse(txtNum1.Text, out num1);
+            int.TryParse(txtNum2.Text, out num2);
+
+            int res = num1 - num2;
+
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int.TryParse(txtNum1.Text, out num1);
+            int.TryParse(txtNum2.Text, out num2);
+
+            int res = num1 * num2;
+
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int.TryParse(txtNum1.Text, out num1);
+            int.TryParse(txtNum2.Text, out num2);
+
+            int res = num1 / num2;
+
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnPromedio_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int.TryParse(txtNum1.Text, out num1);
+            int.TryParse(txtNum2.Text, out num2);
+
+            int res = (num1 + num2) / 2;
+
+            txtResultado.Text = res.ToString();
+        }
+
+        private void btnMenor_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int.TryParse(txtNum1.Text, out num1);
+            int.TryParse(txtNum2.Text, out num2);
+
+            int res = num1 - num2;
+
+            if (res < 0)
+            {
+                txtResultado.Text = "Este es un número negativo (menor a -0)";
+
+            }
+            else
+            {
+                txtResultado.Text = "Es un número positivo (mayor a +0)";
+            }
+        }
+
+        private void btnMayor_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int.TryParse(txtNum1.Text, out num1);
+            int.TryParse(txtNum2.Text, out num2);
+
+            int res = num1 + num2;
+
+            if (res > 0)
+            {
+                txtResultado.Text = "Este es un número positivo (mayor a +0)";
+
+            }
+            else
+            {
+                txtResultado.Text = "Es un número negativo (menor a -0)";
+            }
+        }
+
+        private void btnEQL_Click(object sender, EventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int.TryParse(txtNum1.Text, out num1);
+            int.TryParse(txtNum2.Text, out num2);
+
+            int res = (num1 + num2) / 2;
+
+            txtResultado.Text = res.ToString();
+
+            if (res >= 90 || res == 100)
+            {
+             txtResultado.Text = "Excelente, su calificación es una 'A' :) ";
+            }
+            else if (res <= 89 && res >= 80)
+            {
+             txtResultado.Text = "Felicidades, su calificación es una 'B' ";
+            }
+            else if (res <= 79 && res >= 70)
+            {
+             txtResultado.Text = "Bien, su calificación es una 'C' ";
+            }
+            else if (res <= 69)
+            {
+             txtResultado.Text = "Lo siento, su calificación es una 'F' has reprobado :( ";
+            }
+            else
+            {
+                Console.WriteLine("Este es su equivalente literal");
+            }
+        }
+    }
+}
